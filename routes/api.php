72d65961c5c6ccfde7be3fa58770c68638d3dba8
nandeesh-kundar <nandeesh.n99@gmail.com>
2018-12-17 17:59:18 +0530

    route update

--- routes/api.php
+++ routes/api.php
@@ -138,7 +138,7 @@ Route::prefix('v1')->group(function () {
 
     //Task routes
     Route::group(['middleware' => 'jwt.auth'], function(){
-        
+        Route::get('task/with-users/{id}', 'TaskController@showUsers');
         Route::get('task/project-chart/{id}', 'TaskController@directProjectChart');
         Route::get('task/{id}', 'TaskController@show');
         Route::get('task/by-sprints/{id}', 'TaskController@index');
@@ -147,7 +147,7 @@ Route::prefix('v1')->group(function () {
         Route::get('task/chart/{id}/{status}', 'TaskController@showChart');
         Route::get('task/total-tasks/{id}', 'TaskController@totalTasks');
     });
-    Route::get('task/by-users/{id}', 'TaskController@showUsers');
+   
     //Task member routes
     Route::group(['middleware' => 'jwt.auth'], function(){
         Route::get('task-member/current-assigned-tasks', 'TaskMemberController@getCurrentAssignedTasks');
